e893319927b9dc668a02fa81cf67722385c9d04d
erikpiliposyann <erikpiliposyann@gmail.com>
2026-06-09 23:51:25 +0400

    Implement odd number counting in array
    Count the number of odd integers in an array.

---
+++ Pointer_Array/Tasks.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int n = 5;
+
+	int arr[n];
+	printf("Input %d int elements:\n", n);
+    for(int i = 0; i < n; ++i){
+        scanf("%d", &arr[i]);
+    }
+	int count = 0;
+	for(int i = 0; i < n; ++i){
+		if(arr[i] % 2 == 1){
+			count++;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
